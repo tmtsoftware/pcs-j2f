d0a66a5d0f0d144cb2afaaeb9f96170711f56272
Mitchell Troy <mitchell.troy@jpl.nasa.gov>
2013-11-21 04:07:32 +0000

    update generate.sh
    git-svn-id: file:///Volumes/GitMigration/apssvn/pcs-j2f@396 c866118d-896c-4bc7-9451-62055ac585b8

--- src/main/resources/generate.sh
+++ src/main/resources/generate.sh
@@ -3,7 +3,7 @@
 #verify there is at least one input argument
 if [ $# -eq 0 ]
   then
-   echo "generate.sh [-e or  <dir>] -- program to generate J2F library and java stubs
+   echo "generate.sh [-e or  <fortran dir>, <java source dir> ] -- program to generate J2F library and java stubs
     where:
        -e  Is for generation using eclipse workspace for fortran input and java output
        <dir>  directory with fortran files for input and java output will be in /opt/apps/j2f "
@@ -14,10 +14,12 @@ rm -rf /tmp/tempDir*
 
 if [[ $1 = "-e" ]]; then
    rm /opt/apps/j2f/J*.java
-   java -classpath j2f-0.0.1-SNAPSHOT.jar org.tmt.aps.peas.j2f.J2FCodeGenerator ~/workspace/peas-pcs-fortran/src/
+   cp ~/workspace/pcs-web/src/main/java/org/tmt/aps/peas/lang/interop/RetVal.java $2
+   cp ~/workspace/peas-pcs-fortran/src/structures.f90 $2
+   java -classpath ~/workspace/pcs-j2f/target/j2f-0.0.1-SNAPSHOT.jar org.tmt.aps.peas.j2f.J2FCodeGenerator ~/workspace/peas-pcs-fortran/src/ $2 
    cp /opt/apps/j2f/J*.java ~/workspace/pcs-web/src/main/java/org/tmt/aps/peas/lang/interop/
 else
-   java -classpath j2f-0.0.1-SNAPSHOT.jar org.tmt.aps.peas.j2f.J2FCodeGenerator $1
+   java -classpath j2f-0.0.1-SNAPSHOT.jar org.tmt.aps.peas.j2f.J2FCodeGenerator $1 $2
 fi
 
 
