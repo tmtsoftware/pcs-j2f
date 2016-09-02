cfcd0c0d3c2b80b2da1812a8cfdb2a0ef7b817d7
Scott Michaels <smichaels@tmt.org>
2016-09-02 14:09:17 -0700

    PP-931

---
+++ bin/checkout.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BUILD_DIR=/opt/apps/peasBuild
+GIT_DIR=${BUILD_DIR}/git
+
+# pull into the GIT_DIR subdirectories for J2F and Fortran
+cd ${GIT_DIR}/pcs-j2f
+git pull
+cd ${GIT_DIR}/peas-pcs-fortran
+git pull
+
---
+++ bin/fortran-lib-build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+BUILD_DIR=/opt/apps/peasBuild
+GIT_DIR=${BUILD_DIR}/git
+JAVA_INCLUDE_PATH=/usr/lib/jvm/java-1.7.0-oracle-1.7.0.79.x86_64/include
+
+
+echo "building J2F"
+cd ${GIT_DIR}/pcs-j2f
+mvn clean package
+
+
+echo "building FORTRAN library"
+cd ${GIT_DIR}/peas-pcs-fortran
+export C_INCLUDE_PATH=$JAVA_INCLUDE_PATH:$JAVA_INCLUDE_PATH/linux:$C_INCLUDE_PATH
+cd /opt/apps/j2f
+${GIT_DIR}/pcs-j2f/src/main/resources/generateFromGit.sh /opt/apps/j2f ${GIT_DIR} 
