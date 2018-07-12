1f83a6fbfbb4a570f34806c7bb976e7bf9473a12
smichael1 <smichael1@cox.net>
2018-07-12 21:21:24 +0000

    script changes due to fortran project name change

--- src/main/resources/generateFromGit.sh
+++ src/main/resources/generateFromGit.sh
@@ -12,9 +12,9 @@ fi
 
 rm $1/J*.java
 cp $2/pcs-web/src/main/java/org/tmt/aps/peas/lang/interop/RetVal.java $1
-cp $2/peas-pcs-fortran/src/structures.f90 $1
-cp $2/peas-pcs-fortran/src/logWrite.f90 $1
-java -classpath $2/pcs-j2f/target/j2f-jar-with-dependencies.jar org.tmt.aps.peas.j2f.J2FCodeGenerator $2/peas-pcs-fortran/src/ $1 
+cp $2/pcs-fortran/src/structures.f90 $1
+cp $2/pcs-fortran/src/logWrite.f90 $1
+java -classpath $2/pcs-j2f/target/j2f-jar-with-dependencies.jar org.tmt.aps.peas.j2f.J2FCodeGenerator $2/pcs-fortran/src/ $1 
 cp $1/J*.java $2/pcs-web/src/main/java/org/tmt/aps/peas/lang/interop/
 
 
