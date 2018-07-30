0f599387171c50ee23fa5cfb3280792b422baa9b
Scott Michaels <smichael1@cox.net>
2018-07-30 10:41:25 -0700

    Update checkout.sh
    peas-pcs-fortran changed to pcs-fortran

--- bin/checkout.sh
+++ bin/checkout.sh
@@ -6,6 +6,6 @@ GIT_DIR=${BUILD_DIR}/git
 # pull into the GIT_DIR subdirectories for J2F and Fortran
 cd ${GIT_DIR}/pcs-j2f
 git pull
-cd ${GIT_DIR}/peas-pcs-fortran
+cd ${GIT_DIR}/pcs-fortran
 git pull
 
