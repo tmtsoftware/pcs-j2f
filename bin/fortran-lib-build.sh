c7562b57abb7b7aa6a8f89edd541046d590b82ca
Scott Michaels <smichael1@cox.net>
2018-10-25 13:29:44 -0700

    changed fortran dir name

--- bin/fortran-lib-build.sh
+++ bin/fortran-lib-build.sh
@@ -12,7 +12,7 @@ mvn clean package
 
 
 echo "building FORTRAN library"
-cd ${GIT_DIR}/peas-pcs-fortran
+cd ${GIT_DIR}/pcs-fortran
 export C_INCLUDE_PATH=$JAVA_INCLUDE_PATH:$JAVA_INCLUDE_PATH/linux:$C_INCLUDE_PATH
 cd /opt/apps/j2f
 ${GIT_DIR}/pcs-j2f/src/main/resources/generateFromGit.sh /opt/apps/j2f ${GIT_DIR} 
