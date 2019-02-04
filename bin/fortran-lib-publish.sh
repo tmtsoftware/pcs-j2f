fd157182c18aa7c93a0a482d9fd8512c10dc16df
smichael1 <smichael1@cox.net>
2019-02-04 19:44:59 +0000

    PP-1411

--- bin/fortran-lib-publish.sh
+++ bin/fortran-lib-publish.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 #verify there is at least one input argument
-if [ $# -ne 1 ]
+if [ $# -eq 0 ]
   then
    echo "fortran-lib-publish.sh [-l] [-p version] [-i version]
    where:
-      no arguments deploys the most recent installed build
       -l: lists all installed build versions
-      -p version: builds and publishes specified version, does not install it. (ex: -v 2.1.3")
-      -i version: installs the published version, if no version number is supplied, the latest version is installed (ex: -i 2.1.3)
+      -p version: builds and publishes specified version, does not install it. (ex: -v 2.1.3)
+      -i version: installs the published version, if no version number is supplied, the latest version is installed (ex: -i 2.1.3)"
 
     exit 1
 fi
 
-MAVEN_REPO_HOME=${HOME}/.m2/repository
+MAVEN_REPO_HOME=/home/smichaels/.m2/repository
 BUILD_DIR=/opt/apps/peasBuild
 FORTRAN_LIB_DIR=/opt/apps/lib
 GIT_DIR=${BUILD_DIR}/git
@@ -26,16 +25,21 @@ do
     if [ $var = "-l" ]; then
 	LIST=1
 	elif [ $var = "-i" ]; then
+	NEXT_VAR="INSTALL"
 	INSTALL=1
 	
 	shift
     elif [ $var = "-p" ]; then
 	NEXT_VAR="PUBLISH"
+	PUBLISH=1
 	shift
     else  
         if [ "$NEXT_VAR" == "PUBLISH" ]; then
           VERSION=$var
           NEXT_VAR="" 
+	elif [ "$NEXT_VAR" == "INSTALL" ]; then
+          VERSION=$var
+          NEXT_VAR=""
        fi
     fi
 done
@@ -44,7 +48,7 @@ done
 if [ -z "$LIST" ]; then
    echo ""
 else
-   java -cp $GIT_DIR/pcs-web/target/classes org.tmt.aps.peas.tools.VersionTool -l $MAVEN_REPO_HOME/org/tmt/libpeas
+   java -cp $GIT_DIR/pcs-j2f/target/classes org.tmt.aps.peas.j2f.tools.VersionTool -l $MAVEN_REPO_HOME/org/tmt/libpeas
 fi
 
 # publish
@@ -88,13 +92,13 @@ if [ -z "$INSTALL" ]; then
    echo ""
 else
    if [ -z "$VERSION" ]; then
-      echo "using latest installed version for deploy"
-      VERSION="$(java -cp $GIT_DIR/pcs-web/target/classes org.tmt.aps.peas.tools.VersionTool -v $MAVEN_REPO_HOME/org/tmt/libpeas 2>&1)"
+      echo "using latest published version to install"
+      VERSION="$(java -cp $GIT_DIR/pcs-j2f/target/classes org.tmt.aps.peas.j2f.tools.VersionTool -v $MAVEN_REPO_HOME/org/tmt/libpeas 2>&1)
+"
    fi
 
-   echo $INSTALL
-   
-   cp $MAVEN_REPO_HOME/org/tmt/libpeas/$VERSION/*.so ${FORTRAN_LIB_DIR}/libpeas.so
+   cp ${MAVEN_REPO_HOME}/org/tmt/libpeas/$VERSION/*.so ${FORTRAN_LIB_DIR}/libpeas.so
+
+   echo "Installation of fortran library version $VERSION completed"
    
 fi
-
