c7dc99fa228aa0c46be258d4658a4edfbd09de81
smichael1 <smichael1@cox.net>
2019-02-04 17:44:28 +0000

    PP-1411

--- bin/fortran-lib-publish.sh
+++ bin/fortran-lib-publish.sh
@@ -7,7 +7,7 @@ if [ $# -ne 1 ]
    where:
       no arguments deploys the most recent installed build
       -l: lists all installed build versions
-      -p version: publishes specified version, does not install it. (ex: -v 2.1.3")
+      -p version: builds and publishes specified version, does not install it. (ex: -v 2.1.3")
       -i version: installs the published version, if no version number is supplied, the latest version is installed (ex: -i 2.1.3)
 
     exit 1
@@ -16,8 +16,10 @@ fi
 MAVEN_REPO_HOME=${HOME}/.m2/repository
 BUILD_DIR=/opt/apps/peasBuild
 FORTRAN_LIB_DIR=/opt/apps/lib
-
 GIT_DIR=${BUILD_DIR}/git
+JAVA_INCLUDE_PATH=$JAVA_HOME/include
+
+
 
 for var in "$@"
 do
@@ -53,10 +55,31 @@ else
    if [ -z "$VERSION" ]; then
       echo "no version specified, please specify a version"    
    else
+
+	  echo "renaming current library to libpeas.so.current"
+	  mv ${FORTRAN_LIB_DIR}/libpeas.so ${FORTRAN_LIB_DIR}/libpeas.so.current
+		
+	  echo "building J2F"
+	  cd ${GIT_DIR}/pcs-j2f
+	  mvn clean package
+		
+		
+	  echo "building FORTRAN library"
+	  cd ${GIT_DIR}/pcs-fortran
+	  export C_INCLUDE_PATH=$JAVA_INCLUDE_PATH:$JAVA_INCLUDE_PATH/linux:$C_INCLUDE_PATH
+	  cd /opt/apps/j2f
+	  ${GIT_DIR}/pcs-j2f/src/main/resources/generateFromGit.sh /opt/apps/j2f ${GIT_DIR} 
+
+
       echo $VERSION
       cd $FORTRAN_LIB_DIR
       mvn install:install-file -Dfile=libpeas.so -DgroupId=org.tmt -DartifactId=libpeas -Dversion=$VERSION -Dpackaging=so   
    fi
+   
+   
+   echo "restoring current library to libpeas.so"
+   mv ${FORTRAN_LIB_DIR}/libpeas.so.current ${FORTRAN_LIB_DIR}/libpeas.so   
+   
 fi
 
 
